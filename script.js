719691120b624d8d1ae846c799f0aec35eae78a7
dmuslima <pirmukhamedovamuslima01@gmail.com>
2025-03-24 21:36:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = prompt("Ismingizni kiriting");
+const id = prompt("space idingizni kiriting");
+const password = prompt("Space passwordingizni kiriting");
+
+const user = {
+  name: name,
+  id: id,
+  password: password
+};
+
+console.log(user);
